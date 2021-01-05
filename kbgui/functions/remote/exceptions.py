01c84f55bba3779accf366505225c1c9f6e26f77
alshapton <alshapton@gmail.com>
2021-01-05 22:20:16 +0000

    Corrected typo in exceptions.py
    Summarize the change in less than 50 characters for the title above
    
    This change was made because:
    *
    *
    *
    
    
    
    DELETE BELOW 
    =========
    NOTES:
    
    - Explain the reasons you made this change
    - Make a new bullet for each reason
    - Each line should be under 72 characters
    
    Explain exactly what was done in this commit with more depth than the
    50 character subject line. Remember to wrap at 72 characters!
    
    Include any additional notes, relevant links, or co-authors.

--- kbgui/functions/remote/exceptions.py
+++ kbgui/functions/remote/exceptions.py
@@ -2,6 +2,7 @@
 This module contains the exceptions which the remote calls to the API generate:
 This file is imported as a module and contains the following
 
+
 Exceptions:
     * KBAPIReadTimeOut - if a call to the API times out.
         The timeout respects the timeOut parameter on all functions
